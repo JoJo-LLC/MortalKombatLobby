688ca84982f0ebbe0e7da3e663232df698c38ce8
John Ferretti <Mr.JohnF@outlook.com>
2024-09-13 06:14:05 +0800

    Initial trial: very basic starting point -Run program and you can see

---
+++ MortalKombatLobby/PlayerManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MortalKombatLobby
+{
+    public class PlayerManager
+    {
+        private List<string> loggedInPlayers;
+
+        public PlayerManager()
+        {
+            loggedInPlayers = new List<string>();
+        }
+
+        // Method to check if a username is unique and log in the player
+        public bool LoginPlayer(string username)
+        {
+            if (loggedInPlayers.Contains(username))
+            {
+                return false;  // Username already exists
+            }
+
+            loggedInPlayers.Add(username);
+            return true;  // Login successful
+        }
+
+        // Method to log out a player
+        public void LogoutPlayer(string username)
+        {
+            loggedInPlayers.Remove(username);
+        }
+
+        // Method to return the list of logged-in players
+        public List<string> GetLoggedInPlayers()
+        {
+            return loggedInPlayers;
+        }
+    }
+}
+
--- MortalKombatLobby/Program.cs
+++ MortalKombatLobby/Program.cs
@@ -10,6 +10,33 @@ namespace MortalKombatLobby
     {
         static void Main(string[] args)
         {
+            PlayerManager playerManager = new PlayerManager();
+
+            Console.WriteLine("Welcome to Mortal Kombat Lobby!");
+
+            while (true)
+            {
+                Console.WriteLine("\nEnter your username to log in:");
+                string username = Console.ReadLine();
+
+                if (playerManager.LoginPlayer(username))
+                {
+                    Console.WriteLine($"Welcome, {username}!");
+                    Console.WriteLine("Players currently in the lobby: " + string.Join(", ", playerManager.GetLoggedInPlayers()));
+                }
+                else
+                {
+                    Console.WriteLine("Username already exists. Please choose another one.");
+                }
+
+                Console.WriteLine("\nDo you want to log out? (yes/no)");
+                string logoutResponse = Console.ReadLine();
+                if (logoutResponse.ToLower() == "yes")
+                {
+                    playerManager.LogoutPlayer(username);
+                    Console.WriteLine($"{username} has logged out.");
+                }
+            }
         }
     }
 }
